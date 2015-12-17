39ea995f8b5e3a41cbba748dbdac9210b494f768
xhakaunruly <jalan.henry95@gmail.com>
2015-12-16 19:08:39 -0500

    Create hompage.php

---
+++ hompage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>Home</title>
+	</head>
+	<body>
+		<h1>User's Inbox </h1>
+	
+	Compose new message
+	
+	
+	<div>
+	10 most recent messages
+	</div>
+	
+	</body>
+</html>
